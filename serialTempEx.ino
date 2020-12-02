e0820601164c8b4d6c4b15302388a6fb7cd89d34
KimRiun <56223389+KimRiun@users.noreply.github.com>
2020-12-02 17:36:35 +0900

    고정된 온도로 테스트한 아두이노 파일

---
+++ serialTempEx.ino
@@ -0,0 +1,63 @@
+/* How to use the DHT-22 sensor with Arduino uno
+   Temperature and humidity sensor
+*/
+
+//Libraries
+//#include <DHT.h>;
+
+//Constants
+//#define DHTPIN 8     // what pin we're connected to
+//#define DHTTYPE DHT22   // DHT 22  (AM2302)
+//DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
+
+
+//Variables
+//int chk;
+//float hum;  //Stores humidity value
+float temp; //Stores temperature value
+
+void setup()
+{
+    Serial.begin(115200);
+    pinMode(8, OUTPUT);
+    //dht.begin();
+}
+
+void loop()
+{
+   
+        
+      delay(100);
+      //Read data and store it to variables hum and temp
+      //hum = dht.readHumidity();
+      //temp = dht.readTemperature();
+      temp = 36.5;
+  
+      //Print temp and humidity values to serial monitor
+      /*Serial.print("Humidity: ");
+      Serial.print(hum);
+      Serial.print(" %, Temp: ");
+      Serial.print(temp);
+      Serial.println(" Celsius");
+      */
+      
+     /* Serial.print("Humidity_in_Arduino: ");
+      Serial.print(hum);
+      Serial.println(" %");*/
+      Serial.print("Temperature_in_Arduino: ");
+      Serial.print(temp);
+      Serial.println(" C");
+  
+      // 3 LED
+      if(temp >= 36.5){
+          digitalWrite(8, HIGH);
+          delay(1000);
+      }
+      else{
+          digitalWrite(8, LOW);
+          delay(1000);
+      }
+  
+      delay(300); //Delay 2 sec.
+    
+}
